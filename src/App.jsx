d7adf9acc1b62cba0427e085d4077c8828755429
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-05-10 00:27:32 +0300

    Routing setup and navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Header/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold font-PlayFair text-center py-10">
-        Auto Librarian
-      </h1>
+      <Navbar />
     </>
   );
 }
---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,61 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./Navbar.css";
+const Navbar = () => {
+  const navLinks = (
+    <>
+      <li key="home" className="hover:text-white">
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li key="add-book" className="hover:text-white">
+        <NavLink to="/add-book">Add book</NavLink>
+      </li>
+      <li key="all-book" className="hover:text-white">
+        <NavLink to="/all-book">All Books</NavLink>
+      </li>
+      <li key="borrowed-books" className="hover:text-white">
+        <NavLink to="/borrowed-books">Borrowed Books</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar py-5 md:py-10 md:px-24">
+      <div className="navbar-start">
+        <div className="dropdown text-[#055c36]">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navLinks}
+          </ul>
+        </div>
+        <a className="btn btn-ghost h-full w-40">
+          <img src={logo} alt="Car doctor logo" />
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-[#055c36]">{navLinks}</ul>
+      </div>
+      <div className="navbar-end user flex gap-7">
+        <Link className="btn btn-outline text-[#055c36] px-5">Login</Link>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
