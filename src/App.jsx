03e1949461b62db63590312a6e911bdb9fd6adf9
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-05-10 17:33:12 +0300

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Header/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div>
+        <Outlet />
+      </div>
+      <Footer />
     </>
   );
 }
