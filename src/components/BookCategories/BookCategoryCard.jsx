2dcd483bdd2250fef4387a894734f4be57a4c97d
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-05-12 14:28:27 +0300

    Added categories book in Home page

---
+++ src/components/BookCategories/BookCategoryCard.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+const BookCategoryCard = ({ categoryBook }) => {
+  const { image, category, button } = categoryBook;
+  return (
+    <div className="card shadow-xl border border-[#055c36]">
+      <figure>
+        <img src={image} alt={category} className="rounded-t-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title font-bold font-PlayFair">{category}</h2>
+        <div className="card-actions">
+          <Link
+            to={`/books/category/${category.toLowerCase()}`}
+            className="btn bg-gradient-to-br from-[#055c36] to-[#727d61] text-white"
+          >
+            {button}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default BookCategoryCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,34 @@
 import { useLoaderData } from "react-router";
 import Banner from "../../components/Banner/Banner";
+import { useState } from "react";
+import BookCategoryCard from "../../components/BookCategories/BookCategoryCard";
 
 const Home = () => {
   const data = useLoaderData();
   const books = data.data;
-  console.log(books);
+  const [bookCategory, setBookCategory] = useState(books);
+  // console.log(books);
   return (
     <div>
       <Banner sliderData={books} />
       <div className="max-w-7xl mx-auto px-3 md:px-8 lg:px-14">
-
+        <div className="my-24">
+          <div className="max-w-lg mx-auto text-center space-y-4">
+            <h1 className="text-3xl font-bold font-PlayFair text-center">
+              Categories list of Books
+            </h1>
+            <p>
+              Explore a diverse collection of captivating books spanning various
+              genres, authors, and themes, offering an enriching reading
+              experience for every literary enthusiast.
+            </p>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-10">
+            {bookCategory.map((book) => (
+              <BookCategoryCard key={book._id} categoryBook={book} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
